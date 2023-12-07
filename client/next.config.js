1a9db1f9debdd805cf736cc21dff6ed795142ec9
Arthur CRAHÉ <crahe.arthur@gmail.com>
2023-12-07 19:58:55 +0100

    went back to a classiv configuration

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-       webpack: (config, { isServer }) => {
-              if (!isServer) {
-                     config.resolve.alias['bufferutil'] = false;
-                     config.resolve.alias['utf-8-validate'] = false;
-              }
-              return config;
-       }
+       // webpack: (config, { isServer }) => {
+       //        if (!isServer) {
+       //               config.resolve.alias['bufferutil'] = false;
+       //               config.resolve.alias['utf-8-validate'] = false;
+       //        }
+       //        return config;
+       // }
 }
 
 module.exports = nextConfig;
